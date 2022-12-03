bff45e1ea163656f35a6296afb06aaa6f9a1d08d
Douglas Anderson <douglas.j.anderson@pm.me>
2022-12-03 12:28:36 -0700

    Update run instructions

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,3 @@
 fn main() {
-    println!("Welcome to AOC 2022! Use `cargo run --bin dayN` to run a specific day's challenge");
+    println!("Welcome to AOC 2022! Use `cargo run --bin dayN` to run a specific day's challenge.");
 }
