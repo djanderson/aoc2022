1b2b4ec61bb7214a2c1487d9e6245a2a5a821643
Douglas Anderson <douglas.j.anderson@pm.me>
2022-12-04 13:47:32 -0700

    Day 4 refactor
    Integrating some ideas from https://github.com/avsaase/advent-of-code-2022/blob/master/src/day4.rs

--- src/bin/day4/main.rs
+++ src/bin/day4/main.rs
@@ -1,44 +1,67 @@
 /// Day 4: Camp Cleanup
+use std::cmp::{max, min};
 use std::fs;
-use std::ops::RangeInclusive;
+use std::num::ParseIntError;
+use std::str::FromStr;
 
 pub fn main() {
     let input = fs::read_to_string("input.txt").unwrap();
 
-    let n_contained: i32 = input
+    let n_contained = input
         .lines()
         .map(|line| {
-            let (r1, r2) = line_to_ranges(line);
-            let is_contained = (r1.contains(r2.start()) && r1.contains(r2.end()))
-                || (r2.contains(r1.start()) && r2.contains(r1.end()));
-            is_contained as i32
+            let (r1, r2) = line
+                .split_once(',')
+                .expect("ranges should separated by ','");
+            let s1: SectionRange = r1.parse().unwrap();
+            let s2: SectionRange = r2.parse().unwrap();
+            (s1, s2)
         })
-        .sum();
+        .filter(|(s1, s2)| s1.contains(&s2) || s2.contains(&s1))
+        .count();
 
     println!("Part 1: {}", n_contained);
 
-    let n_overlapped: i32 = input
+    let n_overlapped = input
         .lines()
         .map(|line| {
-            let (r1, r2) = line_to_ranges(line);
-            let is_overlapped = (r1.contains(r2.start()) || r1.contains(r2.end()))
-                || (r2.contains(r1.start()) || r2.contains(r1.end()));
-            is_overlapped as i32
+            let (r1, r2) = line
+                .split_once(',')
+                .expect("ranges should separated by ','");
+            let s1: SectionRange = r1.parse().unwrap();
+            let s2: SectionRange = r2.parse().unwrap();
+            (s1, s2)
         })
-        .sum();
+        .filter(|(s1, s2)| s1.overlaps(&s2))
+        .count();
 
     println!("Part 2: {}", n_overlapped);
 }
 
-// Map each line to a pair of Ranges
-fn line_to_ranges(line: &str) -> (RangeInclusive<i32>, RangeInclusive<i32>) {
-    let bounds: Vec<i32> = line.split([',', '-']).map(|d| d.parse().unwrap()).collect();
-    match bounds[..] {
-        [r1_start, r1_end, r2_start, r2_end] => {
-            let r1 = RangeInclusive::new(r1_start, r1_end);
-            let r2 = RangeInclusive::new(r2_start, r2_end);
-            (r1, r2)
-        }
-        _ => panic!("Invalid input"),
+struct SectionRange {
+    start: i32,
+    end: i32,
+}
+
+impl SectionRange {
+    fn contains(&self, other: &Self) -> bool {
+        self.start <= other.start && self.end >= other.end
+    }
+
+    fn overlaps(&self, other: &Self) -> bool {
+        max(self.start, other.start) <= min(self.end, other.end)
+    }
+}
+
+impl FromStr for SectionRange {
+    type Err = ParseIntError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let (start_str, end_str) = s
+            .split_once('-')
+            .expect("section range should contain dash");
+        let start = start_str.parse().expect("value should parse as an int");
+        let end = end_str.parse().expect("value should parse as an int");
+        Ok(Self { start, end })
     }
 }
