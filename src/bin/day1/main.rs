0106df3dd3dd00c04ffecf188af0ec5aefef1c88
Douglas Anderson <douglas.j.anderson@pm.me>
2022-12-03 21:04:12 -0700

    Refactor day 1 using itertools crate

--- src/bin/day1/main.rs
+++ src/bin/day1/main.rs
@@ -1,37 +1,29 @@
 /// Day 1: Calorie Counting
 use std::fs;
+use std::iter::Iterator;
+
+use itertools::Itertools;
 
 pub fn main() {
     let input = fs::read_to_string("input.txt").unwrap();
 
-    let max_calories: i32 = input
-        .trim()
-        .split("\n\n")
-        .map(|calories| {
-            calories
-                .lines()
-                .map(|s| s.parse::<i32>().expect("line should contain a single integer"))
-                .sum()
-        })
-        .max()
-        .unwrap();
+    let max_calories: i32 = calories_per_elf(&input).max().unwrap();
 
     println!("Part 1: {}", max_calories);
 
-    let mut calories: Vec<i32> = input
-        .trim()
-        .split("\n\n")
-        .map(|calories| {
-            calories
-                .lines()
-                .map(|s| s.parse::<i32>().expect("line should contain a single integer"))
-                .sum()
-        })
-        .collect();
+    let max_3_calories: i32 = calories_per_elf(&input).sorted().rev().take(3).sum();
 
-    calories.sort();
+    println!("Part 2: {}", max_3_calories);
+}
 
-    let top_3: i32 = calories.iter().rev().take(3).sum();
-
-    println!("Part 2: {}", top_3);
+fn calories_per_elf(input: &str) -> impl Iterator<Item = i32> + '_ {
+    input.trim().split("\n\n").map(|calories| {
+        calories
+            .lines()
+            .map(|s| {
+                s.parse::<i32>()
+                    .expect("line should contain a single integer")
+            })
+            .sum()
+    })
 }
