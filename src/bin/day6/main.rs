7dda2fef8930ad6caa0579a252378fd007f1bdb9
Douglas Anderson <douglas.j.anderson@pm.me>
2022-12-08 19:03:24 -0700

    Refactor day 6
    Neat .as_bytes().windows() idea from https://github.com/avsaase/advent-of-code-2022/blob/master/src/day6.rs

--- src/bin/day6/main.rs
+++ src/bin/day6/main.rs
@@ -5,37 +5,30 @@ use std::fs;
 pub fn main() {
     let input = fs::read_to_string("input.txt").unwrap();
 
-    let marker_idx = find_marker(&input, 4);
-    let marker_loc = 1 + marker_idx.expect("input should contain start-of-packet marker");
+    let marker_loc = find_marker(&input, 4).expect("input should contain start-of-packet marker");
 
     println!("Part 1: {}", marker_loc);
 
-    let marker_idx = find_marker(&input, 14);
-    let marker_loc = 1 + marker_idx.expect("input should contain start-of-packet marker");
+    let marker_loc = find_marker(&input, 14).expect("input should contain start-of-packet marker");
 
     println!("Part 2: {}", marker_loc);
 }
 
 fn find_marker(input: &str, window_size: usize) -> Option<usize> {
-    let mut window: Vec<u8> = vec![0; window_size];
+    let mut set = HashSet::with_capacity(window_size);
 
-    let mut marker_idx: Option<usize> = None;
-    for (i, b) in input.bytes().enumerate() {
-        window[i % window_size] = b;
-        if i < 3 {
-            continue;
-        }
-        if is_unique(&window) {
-            marker_idx = Some(i);
-            break;
-        }
-    }
-
-    marker_idx
+    input
+        .as_bytes()
+        .windows(window_size)
+        .enumerate()
+        .find_map(|(i, window)| match is_unique(window, &mut set) {
+            true => Some(i + window_size),
+            false => None,
+        })
 }
 
-fn is_unique(arr: &[u8]) -> bool {
-    let mut set = HashSet::with_capacity(arr.len());
+fn is_unique(arr: &[u8], set: &mut HashSet<u8>) -> bool {
+    set.clear();
     let mut unique = true;
     for b in arr {
         if !set.insert(*b) {
