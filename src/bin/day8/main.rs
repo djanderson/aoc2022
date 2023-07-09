da9ac743494069a7bb030a9d7b73bb44b285b8c4
Douglas Anderson <douglas.j.anderson@pm.me>
2023-07-08 19:49:01 -0600

    Fix lint of day8
    Also got lucky and was misunderstood what u8::from(byte) was doing, I
    thought it was parsing the u8 number from the ascii byte, but it was not
    doing anything. In the end, only relative differences mattered for this
    problem, so it worked, but I did fix the parsing logic to actually parse
    the number, just to be less "clever".

--- src/bin/day8/main.rs
+++ src/bin/day8/main.rs
@@ -3,8 +3,8 @@ use std::fs;
 
 pub fn main() {
     let input = fs::read_to_string("input.txt").unwrap();
-    let mut rows: Vec<Vec<u8>> = vec![];
-    let mut cols: Vec<Vec<u8>> = vec![];
+    let mut rows: Vec<Vec<u32>> = vec![];
+    let mut cols: Vec<Vec<u32>> = vec![];
 
     // Initialize an empty cols vector for each number in the first row.
     input
@@ -15,8 +15,9 @@ pub fn main() {
         .for_each(|_| cols.push(vec![]));
 
     // Initialize the rows and cols vectors.
+    const BASE10: u32 = 10;
     for line in input.lines() {
-        let row: Vec<u8> = line.bytes().map(|b| u8::from(b)).collect();
+        let row: Vec<u32> = line.chars().map(|c| c.to_digit(BASE10).unwrap()).collect();
         for (i, height) in row.iter().enumerate() {
             cols[i].push(*height);
         }
@@ -29,14 +30,13 @@ pub fn main() {
     // Part 2
     let score = best_scenic_score(&rows, &cols);
     println!("Part 2: {}", score);
-
 }
 
 // Part 2: Find the most scenic view.
-fn best_scenic_score(rows: &Vec<Vec<u8>>, cols: &Vec<Vec<u8>>) -> usize {
+fn best_scenic_score(rows: &Vec<Vec<u32>>, cols: &Vec<Vec<u32>>) -> usize {
     let mut scores: Vec<usize> = vec![];
 
-    fn score_view<'a>(this: u8, others: impl Iterator<Item = &'a u8>) -> usize {
+    fn score_view<'a>(this: u32, others: impl Iterator<Item = &'a u32>) -> usize {
         let mut score = 0;
         for other in others {
             score += 1;
@@ -49,13 +49,13 @@ fn best_scenic_score(rows: &Vec<Vec<u8>>, cols: &Vec<Vec<u8>>) -> usize {
 
     // Don't consider edge trees, since a score on one edge of 0 makes the
     // total scenic score 0.
-    for i in 1..rows.len() - 1 {
-        for j in 1..cols.len() - 1 {
-            let this_tree = rows[i][j];
-            let score_left = score_view(this_tree, rows[i][..j].iter().rev());
-            let score_right = score_view(this_tree, rows[i][j + 1..].iter());
-            let score_up = score_view(this_tree, cols[j][..i].iter().rev());
-            let score_down = score_view(this_tree, cols[j][i + 1..].iter());
+    for (i, row) in rows.iter().enumerate().take(rows.len() - 1).skip(1) {
+        for (j, col) in cols.iter().enumerate().take(cols.len() - 1).skip(1) {
+            let this_tree = row[j];
+            let score_left = score_view(this_tree, row[..j].iter().rev());
+            let score_right = score_view(this_tree, row[j + 1..].iter());
+            let score_up = score_view(this_tree, col[..i].iter().rev());
+            let score_down = score_view(this_tree, col[i + 1..].iter());
             scores.push(score_left * score_right * score_up * score_down);
         }
     }
@@ -64,33 +64,33 @@ fn best_scenic_score(rows: &Vec<Vec<u8>>, cols: &Vec<Vec<u8>>) -> usize {
 }
 
 // Part 1: Find the number of trees that are visible from outside.
-fn visible_trees(rows: &Vec<Vec<u8>>, cols: &Vec<Vec<u8>>) -> usize {
+fn visible_trees(rows: &Vec<Vec<u32>>, cols: &Vec<Vec<u32>>) -> usize {
     // All of the outside perimeter are visible.
     let mut nvisible = rows.len() * 2 + (cols.len() - 2) * 2;
 
-    for i in 1..rows.len() - 1 {
-        for j in 1..cols.len() - 1 {
-            let height = rows[i][j];
+    for (i, row) in rows.iter().enumerate().take(rows.len() - 1).skip(1) {
+        for (j, col) in cols.iter().enumerate().take(cols.len() - 1).skip(1) {
+            let height = row[j];
             // Check left.
-            let visible_left = rows[i][..j].iter().all(|&h| h < height);
+            let visible_left = row[..j].iter().all(|&h| h < height);
             if visible_left {
                 nvisible += 1;
                 continue;
             }
             // Check right.
-            let visible_right = rows[i][j + 1..].iter().all(|&h| h < height);
+            let visible_right = row[j + 1..].iter().all(|&h| h < height);
             if visible_right {
                 nvisible += 1;
                 continue;
             }
             // Check up.
-            let visible_up = cols[j][..i].iter().all(|&h| h < height);
+            let visible_up = col[..i].iter().all(|&h| h < height);
             if visible_up {
                 nvisible += 1;
                 continue;
             }
             // Check down.
-            let visible_down = cols[j][i + 1..].iter().all(|&h| h < height);
+            let visible_down = col[i + 1..].iter().all(|&h| h < height);
             if visible_down {
                 nvisible += 1;
                 continue;
