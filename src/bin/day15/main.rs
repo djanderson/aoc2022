6fe6c8f80d0ed0148265a25e3e1efa3956f2804d
Douglas Anderson <douglas.j.anderson@pm.me>
2023-08-21 18:40:38 -0600

    Add day15 part 2

--- src/bin/day15/main.rs
+++ src/bin/day15/main.rs
@@ -4,6 +4,18 @@ use std::fs;
 use std::ops::RangeInclusive;
 use std::str::FromStr;
 
+pub fn main() {
+    let input = fs::read_to_string("input.txt").unwrap();
+
+    let telemetry: Vec<_> = input
+        .lines()
+        .map(|l| Telemetry::from_str(l).unwrap())
+        .collect();
+
+    println!("Part 1: {}", part1(&telemetry, 2_000_000));
+    println!("Part 2: {}", part2(&telemetry));
+}
+
 // Algorithm:
 //
 // 1. Scan through input and determine (min, max) value of X
@@ -13,16 +25,7 @@ use std::str::FromStr;
 //
 // NOTE: The number of sensors is very small, so there is no benefit to merging the ranges
 //       covered.
-pub fn main() {
-    let input = fs::read_to_string("input.txt").unwrap();
-
-    let telemetry: Vec<_> = input
-        .lines()
-        .map(|l| Telemetry::from_str(l).unwrap())
-        .collect();
-
-    let target_y: i32 = 2000000;
-
+fn part1(telemetry: &[Telemetry], target_y: i32) -> i32 {
     // Find the range of x covered by each sensor at row `target_y`
     let mut min_x = i32::MAX;
     let mut max_x = i32::MIN;
@@ -30,7 +33,7 @@ pub fn main() {
     let mut target_line_beacons: BTreeSet<i32> = BTreeSet::new();
 
     for pkt in telemetry.iter() {
-        let mdist = pkt.manhattan_distance() as i32;
+        let mdist = pkt.distance as i32;
         let ydist = pkt.sensor.y.abs_diff(target_y) as i32;
         let xdist = mdist - ydist;
         if xdist < 0 {
@@ -58,10 +61,52 @@ pub fn main() {
         }
     }
 
-    println!("Part 1: {count}");
+    count
 }
 
-#[derive(Debug, PartialEq)]
+// Algorithm:
+//
+// The search space 4000000 * 4000000 is far too large to brute force, but we can
+// reduce the search space by realizing that if there is exactly one point in [0, 4000000]
+// that is not covered by any sensor, than this point must necessarily be on the boundary
+// of multiple sensors.
+//
+// 1. Create an iterator over the "boundary" of a sensor, i.e., all the points 1 step
+//    farther than the distance to a sensor's closest beacon.
+// 2. For each boundary point of each sensor, determine whether another sensor would have detected
+//    a beacon there by calulating the distance between that point and each other sensor.
+// 3. If any boundary point is not covered by any other sensor, it must be the distress beacon.
+fn part2(telemetry: &[Telemetry]) -> isize {
+    fn in_bounds(p: &Point) -> bool {
+        p.x >= 0 && p.y >= 0 && p.x <= 4000000 && p.y <= 4000000
+    }
+    for (i, this) in telemetry.iter().enumerate() {
+        let boundary = SensorBoundary::new(this.sensor.clone(), this.distance);
+        for point in boundary {
+            if !in_bounds(&point) {
+                continue;
+            }
+            let mut covered = false;
+            let other_sensors =
+                telemetry
+                    .iter()
+                    .enumerate()
+                    .filter_map(|(j, s)| if j != i { Some(s) } else { None });
+            for other in other_sensors {
+                if other.sensor.manhattan_distance(&point) <= other.distance {
+                    covered = true;
+                    break;
+                }
+            }
+            if !covered {
+                return point.x as isize * 4000000 + point.y as isize;
+            }
+        }
+    }
+    panic!("Not found");
+}
+
+#[derive(Clone, Debug, PartialEq)]
 struct Point {
     x: i32,
     y: i32,
@@ -99,12 +144,7 @@ impl FromStr for Point {
 struct Telemetry {
     sensor: Point,
     beacon: Point,
-}
-
-impl Telemetry {
-    fn manhattan_distance(&self) -> u32 {
-        self.sensor.manhattan_distance(&self.beacon)
-    }
+    distance: u32,
 }
 
 impl FromStr for Telemetry {
@@ -118,7 +158,130 @@ impl FromStr for Telemetry {
         let sensor = Point::from_str(sensor_s)?;
         let beacon_s = &beacon_telem[beacon_telem.find('x').ok_or(ParseTelemetryError)?..];
         let beacon = Point::from_str(beacon_s)?;
+        let distance = sensor.manhattan_distance(&beacon);
 
-        Ok(Telemetry { sensor, beacon })
+        Ok(Telemetry {
+            sensor,
+            beacon,
+            distance,
+        })
+    }
+}
+
+/// An iterator for points around (just outside) a sensor's boundary
+#[derive(Debug)]
+struct SensorBoundary {
+    sensor: Point,
+    distance: u32,
+    current: Option<Point>,
+    size: usize,
+}
+
+impl SensorBoundary {
+    fn new(sensor: Point, distance: u32) -> Self {
+        SensorBoundary {
+            sensor,
+            distance,
+            current: None,
+            // Consider distance to left and right (x2), above and below each (x2)
+            // and directly left, top, bottom, and right of center.
+            size: distance as usize * 4 + 4,
+        }
+    }
+}
+
+/// Iterate points around a sensor boundary.
+///
+/// Given a sensor (S) and beacon (B) in the following configuration (distance 1),
+/// the perimeter (P) will move from left to right, heigh to low, as follows:
+///
+/// .....    .....    .....    ..P..    .....    .....    .....    .....
+/// ..#..    .P#..    ..#..    ..#..    ..#..    ..#P.    ..#..    ..#..
+/// P#SB. -> .#SB. -> .#SB. -> .#SB. -> .#SB. -> .#SB. -> .#SB. -> .#SBP
+/// ..#..    ..#..    .P#..    ..#..    ..#..    ..#..    ..#P.    ..#..
+/// .....    .....    .....    .....    ..P..    .....    .....    .....
+///
+impl Iterator for SensorBoundary {
+    type Item = Point;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let Point {
+            x: sensor_x,
+            y: sensor_y,
+        } = self.sensor;
+        if self.size == 0 {
+            return None;
+        }
+
+        self.size -= 1;
+
+        let distance = self.distance as i32;
+
+        // First perimeter point
+        let Some(Point { x: mut current_x, y: mut current_y }) = self.current else {
+            let current = Point { x: sensor_x - distance - 1, y: sensor_y };
+            self.current = Some(current.clone());
+            return Some(current);
+        };
+
+        if current_y < sensor_y {
+            current_y = sensor_y + (distance - sensor_x.abs_diff(current_x) as i32) + 1;
+        } else {
+            current_x += 1;
+            current_y = sensor_y - (distance - sensor_x.abs_diff(current_x) as i32) - 1;
+        }
+
+        self.current = Some(Point {
+            x: current_x,
+            y: current_y,
+        });
+        self.current.clone()
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        (self.size, Some(self.size))
+    }
+}
+
+impl ExactSizeIterator for SensorBoundary {}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn boundary_values() {
+        let s = "Sensor at x=2, y=2: closest beacon is at x=3, y=2";
+        let t = Telemetry::from_str(s).unwrap();
+        let b = SensorBoundary::new(t.sensor, t.distance);
+        let actual: Vec<Point> = b.collect();
+        let expected = vec![
+            Point { x: 0, y: 2 },
+            Point { x: 1, y: 1 },
+            Point { x: 1, y: 3 },
+            Point { x: 2, y: 0 },
+            Point { x: 2, y: 4 },
+            Point { x: 3, y: 1 },
+            Point { x: 3, y: 3 },
+            Point { x: 4, y: 2 },
+        ];
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn boundary_len() {
+        let s = "Sensor at x=2, y=2: closest beacon is at x=3, y=2";
+        let t = Telemetry::from_str(s).unwrap();
+        let mut b = SensorBoundary::new(t.sensor, t.distance);
+        assert_eq!(b.len(), 8);
+        _ = b.next();
+        assert_eq!(b.len(), 7);
+        for _ in 0..6 {
+            _ = b.next();
+        }
+        assert_eq!(b.len(), 1);
+        assert!(b.next().is_some());
+        assert!(b.next().is_none());
+        assert_eq!(b.len(), 0);
     }
 }
